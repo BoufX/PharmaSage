dcb4fcc3b7bfa38cb67c337a8bf4898de021aeb3
abdallah boufrad <abdallahboufrad@gmail.com>
2023-08-31 15:51:59 +0100

    map_changes

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -1,14 +1,14 @@
 class PharmaciesController < ApplicationController
     def index 
         @pharmacies = Pharmacy.all
-        @on_guard_pharmacies = filter_on_guard_pharmacies(@pharmacies)
+        # @on_guard_pharmacies = filter_on_guard_pharmacies(@pharmacies)
         end
       
        def show
         @pharmacy = Pharmacy.find(params[:id])
         end
       
-        def filter_on_guard_pharmacies(pharmacies)
-          pharmacies.select { |pharmacy| pharmacy.on_guard }
-        end
+        # def filter_on_guard_pharmacies(pharmacies)
+        #   pharmacies.select { |pharmacy| pharmacy.on_guard }
+        # end
 end
