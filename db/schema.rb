4d73f11536fac8edd4dc449e74efe30572531ec5
Oussama El Boukioty <oelboukioty@gmail.com>
2023-09-06 17:28:50 +0100

    fix issues

--- db/schema.rb
+++ db/schema.rb
@@ -91,7 +91,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_214323) do
     t.string "last_name"
     t.string "nickname"
     t.boolean "admin", default: false, null: false
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
